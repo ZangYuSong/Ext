d76f3085b7152d8a6bff9f40565c681181fcc3cd
zys <361464>
2017-01-24 11:33:45 +0800

    重构app/controller/tree/Tree.js代码

--- Ext/app/controller/tree/Tree.js
+++ Ext/app/controller/tree/Tree.js
@@ -13,47 +13,46 @@
         if (node.get("cls") != "") {
             ischecked = true;
         }
-        checkedChildNode(node, ischecked);
-        checkedParentNode(node, ischecked);
-    }
-});
-
-// 选中所有的子节点
-var checkedChildNode = function (node, ischecked) {
-    node.set("checked", ischecked);
-    node.set("cls", "");
-    if (node.hasChildNodes()) {     // 是否包含子节点
-        node.eachChild(function (nodeChild) {
-            checkedChildNode(nodeChild, ischecked)
-        });
-    }
-}
-
-// 选中所有的父节点，半选或者选中
-// 在Ext4.0之后，树的选定状态就只有选定和不选定，取消了半选定状态。
-// 因此在设计树的时候应该只给叶子节点(leaf)设置checked属性
-// 如果一定要使用半选定状态可以参考下述的代码
-var checkedParentNode = function (node, ischecked) {
-    var parent = node.parentNode;
-    var partial = true;
-    if (parent != null) {
-        parent.eachChild(function (parentChild) {
-            // 确定半选状态
-            // 情况一：子节点的checked与ischecked不一致
-            // 情况二：子节点为半选状态 parentChild.get("cls") != ""
-            if (parentChild.get("checked") != ischecked || parentChild.get("cls") != "") {
-                // 半选定状态为 : 选定 + 透明度50%
-                parent.set("cls", "partial_select");
-                parent.set("checked", true);
-                partial = false;
-                return false;
-            }
-        });
-        if (partial) {
-            parent.set("checked", ischecked);
-            parent.set("cls", "");
+        this.checkedChildNode(node, ischecked);
+        this.checkedParentNode(node, ischecked);
+    },
+    // 选中所有的子节点
+    checkedChildNode: function (node, ischecked) {
+        node.set("checked", ischecked);
+        node.set("cls", "");
+        if (node.hasChildNodes()) {     // 是否包含子节点
+            node.eachChild(function (nodeChild) {
+                this.checkedChildNode(nodeChild, ischecked)
+            });
         }
-        checkedParentNode(parent, ischecked);
+    },
+    // 选中所有的父节点，半选或者选中
+    // 在Ext4.0之后，树的选定状态就只有选定和不选定，取消了半选定状态。
+    // 因此在设计树的时候应该只给叶子节点(leaf)设置checked属性
+    // 如果一定要使用半选定状态可以参考下述的代码
+    checkedParentNode: function (node, ischecked) {
+        var parent = node.parentNode;
+        var partial = true;
+        if (parent != null) {
+            parent.eachChild(function (parentChild) {
+                // 确定半选状态
+                // 情况一：子节点的checked与ischecked不一致
+                // 情况二：子节点为半选状态 parentChild.get("cls") != ""
+                if (parentChild.get("checked") != ischecked || parentChild.get("cls") != "") {
+                    // 半选定状态为 : 选定 + 透明度50%
+                    parent.set("cls", "partial_select");
+                    parent.set("checked", true);
+                    partial = false;
+                    return false;
+                }
+            });
+            if (partial) {
+                parent.set("checked", ischecked);
+                parent.set("cls", "");
+            }
+            this.checkedParentNode(parent, ischecked);
+        }
     }
-}
+
+});
 
